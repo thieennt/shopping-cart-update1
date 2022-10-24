b05c02686dd3fac11979a592c98bbfd608197d8c
Thien Nguyen <thieennt@gmail.com>
2022-10-24 08:13:25 +0700

    update something

--- src/redux/slice/cartSlice.ts
+++ src/redux/slice/cartSlice.ts
@@ -1,13 +1,11 @@
-import { SmileOutlined } from "@ant-design/icons";
 import { createSlice } from "@reduxjs/toolkit";
 import { notification } from "antd";
-import type { NotificationPlacement } from "antd/es/notification";
-import React from "react";
 
 interface ICart {
   id: number;
   name: string;
-  img: string;
+  image: string;
+  price: number;
   cartQuantity: number;
 }
 
@@ -18,7 +16,10 @@ export type Cart = {
 };
 
 const initialState: Cart = {
-  cartItems: [],
+  cartItems:
+    typeof window !== "undefined" && localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart") as string)
+      : [],
   cartTotalQuantity: 0,
   cartTotalAmount: 0,
 };
@@ -37,19 +38,96 @@ const cartSlice = createSlice({
           ...state.cartItems[existingIndex],
           cartQuantity: state.cartItems[existingIndex].cartQuantity + 1,
         };
-        notification.open({
+        notification.success({
+          message: "Increased product successfully !",
+        });
+      } else {
+        let tempProductItem = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(tempProductItem);
+        notification.success({
           message: "Add to cart successfully !",
         });
       }
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
     },
-    decreaseCart(state, action) {},
-    removeFromCart(state, action) {},
-    getTotals(state, action) {},
-    clearCart(state, action) {},
+
+    decreaseProduct(state, action) {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+
+        notification.success({
+          message: "Decreased product successfully !",
+        });
+      } else if (state.cartItems[itemIndex].cartQuantity === 1) {
+        const nextCartItems = state.cartItems.filter(
+          (item) => item.id !== action.payload.id
+        );
+
+        state.cartItems = nextCartItems;
+
+        notification.success({
+          message: "Producted removed successfully !",
+        });
+      }
+
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+    },
+
+    removeFromCart(state, action) {
+      state.cartItems.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          const nextCartItems = state.cartItems.filter(
+            (item) => item.id !== action.payload.id
+          );
+
+          state.cartItems = nextCartItems;
+
+          notification.success({
+            message: "Producted removed successfully !",
+          });
+        }
+        localStorage.setItem("cart", JSON.stringify(state.cartItems));
+        return state;
+      });
+    },
+    getTotal(state) {
+      let { total, quantity } = state.cartItems.reduce(
+        (cartTotal, cartItem) => {
+          const { price, cartQuantity } = cartItem;
+          const itemTotal = price * cartQuantity;
+
+          cartTotal.total += itemTotal;
+          cartTotal.quantity += cartQuantity;
+
+          return cartTotal;
+        },
+        { total: 0, quantity: 0 }
+      );
+
+      state.cartTotalQuantity = quantity;
+      state.cartTotalAmount = total;
+    },
+    clearCart(state) {
+      state.cartItems = [];
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+
+      notification.success({
+        message: "Cart cleared !",
+      });
+    },
   },
 });
 
-export const { addToCart, decreaseCart, removeFromCart, getTotals, clearCart } =
-  cartSlice.actions;
+export const {
+  addToCart,
+  decreaseProduct,
+  removeFromCart,
+  getTotal,
+  clearCart,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
